d0194bfe6c3457e5eb83162e5ebed7d697e21850
Shah <sharafat@quizell.com>
2025-07-08 07:33:55 +0500

    daily commission command  added in schedule to run every day

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+Schedule::macro('scheduleDailyTasks', function () {
+    $this->command('app:calculate-daily-income')->dailyAt('00:30'); // or ->daily()
+});
