a581d702fe0060fe226e762baa429d85772fc919
Shah <sharafat@quizell.com>
2025-07-15 14:49:47 +0500

    user resource updated

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -22,7 +22,9 @@ class UserResource extends JsonResource
             'is_admin' => $this->is_admin,
             'phone' => $this->phone,
             'withdrawal_address'=>$this->withdrawal_address,
-            'avatar' => $this->avatar ? asset('storage/'.$this->avatar) : null,
+            'avatar' =>$this->avatar
+            ? asset('uploads/' . $this->avatar)
+            : null,
             'referral_code' => $this->referral_code,
             'created_at' => $this->created_at,
             'wallet' => WalletResource::make($this->whenLoaded('wallet')),
